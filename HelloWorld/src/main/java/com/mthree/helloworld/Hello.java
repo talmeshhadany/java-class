5606fa7f05252adf6aef2e6cbfc80e07f68dba11
Talmeshhadany <talmeshhadany@hotmail.com>
2022-02-15 10:25:29 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,18 @@
+
+/**
+ *Java-Class
+ * @author Thamer AL-Meshhadany
+ * email: talmeshhadany@hotmail.com
+ * date : current date
+ * purpose: My first Hello World program in an IDE
+ */
+
+package com.mthree.helloworld;
+
+public class Hello {
+    public static void main(String args[]){
+    
+    System.out.println("Hello, World!");
+    }
+    
+}
