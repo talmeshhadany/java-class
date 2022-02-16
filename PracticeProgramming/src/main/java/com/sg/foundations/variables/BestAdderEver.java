cf7c3e98686ea42b0db053b6748984af46226e08
talmeshhadany <talmeshhadany@hotmail.com>
2022-02-16 17:08:39 -0500

    PracticeProgramming in NetBeans

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author ta
+ */
+public class BestAdderEver {
+    public static void main(String args[]){
+    String stringNumber1="10"; 
+    String stringNumber2="20";
+    String stringNumber3="30";
+    int sum = Integer.parseInt(stringNumber1) + Integer.parseInt(stringNumber2) + Integer.parseInt(stringNumber3);
+    System.out.println(stringNumber1 + " + " + stringNumber2 + " + " + stringNumber3 + " = " + sum);
+    
+    }
+}
